3aa54ddd352c0ba4f26a3616cdbd227e45bf1cd1
Christina <christinajaiello@gmail.com>
2017-11-04 21:39:25 -0400

    I think I'm getting closer

--- app.py
+++ app.py
@@ -209,10 +209,12 @@ def recording():
 def transcribe():
     print("Transcribe:")
     print(request)
-    print(request.args.get('RecordingSid', '')
-    print(request.args.get('TranscriptionText', '')
-    print(request.args.get('TranscriptionSid', '')
-    print(request.args.get('TranscriptionStatus', '')
+    print(request.args.get('RecordingSid', ''))
+    print(request.args.get('TranscriptionText', ''))
+    print(request.args.get('TranscriptionSid', ''))
+    print(request.args.get('TranscriptionStatus', ''))
+    print(request.json.get('sid', '')
+    print(request.json.get('transcription_text', '')
     return(str(request))
 
 
